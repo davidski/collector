128a88ffaa84b5cf1592bd38a5f096c83cea0ba7
David F. Severski <davidski@deadheaven.com>
2019-04-07 14:49:10 -0700

    Create validation function for tidyrisk_question_set

--- R/tidyrisk_question_set.R
+++ R/tidyrisk_question_set.R
@@ -5,7 +5,8 @@ setOldClass("tidyrisk_question_set")
 #'
 #' \code{new.tidyrisk_question_set} is a low-level constructor that takes a list of dataframes.
 #' \code{tidyrisk_question_set} constructs a tidyrisk_question_set object from dataframes.
-#' \code{as.blob} is a S3 generic that converts existing objects.
+#' \code{as.tidyrisk_question_set} is a S3 generic that converts existing objects.
+#' \code{validate_tidyrisk_question_set} verifies that the data elements are internally consistent.
 #'
 #' @param ... Individual dataframes
 #' @param domains Domains
@@ -20,9 +21,12 @@ setOldClass("tidyrisk_question_set")
 #' NULL
 tidyrisk_question_set <- function(domains, scenarios, capabilities, calibration,
                       expertise, threat_communities) {
-  new_tidyrisk_question_set(list(domains = domains, scenarios = scenarios,
-                     capabilities = capabilities, calibration = calibration,
-                     expertise = expertise, threat_communities = threat_communities))
+  x <- new_tidyrisk_question_set(list(domains = domains, scenarios = scenarios,
+                                      capabilities = capabilities,
+                                      calibration = calibration,
+                                      expertise = expertise,
+                                      threat_communities = threat_communities))
+  validate_tidyrisk_question_set(x)
 }
 
 #' @export
@@ -64,3 +68,33 @@ as.data.frame.tidyrisk_question_set <- function(x, ...) {
 is_tidyrisk_question_set <- function(x) {
   inherits(x, "tidyrisk_question_set")
 }
+
+#' @export
+#' @rdname tidyrisk_question_set
+validate_tidyrisk_question_set <- function(x) {
+
+  enforce_tidyrisk_question_set(x)
+
+  # check that there is agreement between domains/scenarios/capabilities
+  domain_list <- unique(x$domains$domain_id)
+  scenario_list <- unique(x$scenarios$domain_id)
+  capability_list <- unique(x$capabilities$domain_id)
+
+  if (!setequal(domain_list, scenario_list)) {
+    stop("Scenarios and domains disagree.", call. = FALSE)
+  }
+  if (!setequal(domain_list, capability_list)) {
+    stop("capability and domains disagree.", call. = FALSE)
+  }
+
+  # look for agreement in threat communities
+  threat_list <- unique(x$threat_communities$threat_id)
+  scenario_list <- unique(x$scenarios$threat_id)
+  if (!setequal(threat_list, scenario_list)) {
+    stop("threats and scenarios disagree.", call. = FALSE)
+  }
+
+  x
+
+}
+
